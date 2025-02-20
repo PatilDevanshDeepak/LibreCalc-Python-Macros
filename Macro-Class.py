24b1e032e8a4e8ec6f6dfc2d3d2101ba00146e65
Devansh Deepak Patil <119884280+PatilDevanshDeepak@users.noreply.github.com>
2025-02-20 15:56:03 -0500

    Update Macro-Class.py

--- Macro-Class.py
+++ Macro-Class.py
@@ -5,23 +5,26 @@ class WorkBook:
 		self.doc = XSCRIPTCONTEXT.getDocument()
 		self.active = self.doc.CurrentController
 		self.ActiveSheet = self.getActiveSheet()
-		self.ActiveCell = self.getActiveCell()
+		self.ActiveCell = self.getActiveSelection()
 		
 	def Select(self, oCell):
 		self.active.select(oCell)
-		self.ActiveCell = self.getActiveCell() 
+		self.ActiveCell = oCell.getCellByPosition(0 , 0)
 		
 	def getActiveSheet(self):
 		return self.active.getActiveSheet()
 		
-	def getActiveCell(self):
+	def getActiveSelection(self):
 		return self.active.getSelection()
 	
 	def Cell(self, row, col):
-		return (self.ActiveSheet.getCellByPosition(col-1, row-1))
+		return self.ActiveSheet.getCellByPosition(col-1, row-1)
 		
-	def Range(self, rangeName):
-		return (self.ActiveSheet.getCellRangeByName(rangeName))
+	def Range(self, rangeName=None , fromCell=None , toCell=None):
+		if self.isRangeNameGiven(isRowIndex=fromCell , isColumnIndex=toCell , isRangeName=rangeName):
+			return self.ActiveSheet.getCellRangeByName(cellName)
+		else:
+			return self.ActiveSheet.getCellRangeByName(fromCell.AbsoluteName+":"+toCell.AbsoluteName)
 		
 	def Offset(self, rowIndex, colIndex):
 		row = self.ActiveCell.RangeAddress.StartRow + rowIndex
@@ -37,11 +40,25 @@ class WorkBook:
 	def MsgBox(self, message, title="Information", msgBoxType=0):
 		self.active.getFrame().getComponentWindow().showMessageBox(message, title, msgBoxType)
 
+	def isRangeNameGiven(self, isRowIndex=None , isColumnIndex=None , isRangeName=None):
+		if isRangeName!=None and (isRowIndex==None and isColumnIndex==None):
+			return True
+		elif isRangeName==None and (isRowIndex!=None and isColumnIndex!=None):
+			return False
+
+	def isNull(self, rowIndex=None , columnIndex=None , rangeName=None):
+		if self.isRangeNameGiven(isRowIndex=rowIndex , isColumnIndex=columnIndex , isRangeName=rangeName):
+			return True if len(self.Range(rangeName).getString())==0 else False
+		else:
+			return True if len(self.Cell(rowIndex , columnIndex).getString())==0 else False
+
+
+
 
 Calc = WorkBook()
 
 def Automate():
     # edit as per your requirement
-	Calc.Select(Calc.Cell(1 , 1)) # Select Cell A1
-	Calc.ActiveCell.setString("Hello World")
+	Calc.Select(Calc.Range(fromCell=Calc.Cell(1 , 1) , toCell=Calc.Cell(5 , 5)))
+	Calc.ActiveCell.setString("Hello")
 
